e60c14f88995b4c2a194100f9144541cc9986f28
Seoweol <ahddl622@naver.com>
2024-02-08 20:29:50 +0900

    test sadawdadwdawdawdawdawd

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,7 +4,7 @@ import { TodoHeader } from "../../style/TodoStyle";
 function Header() {
   return (
     <TodoHeader>
-      <h1>My Todo List!</h1>
+      <h1>My Todo List</h1>
       <h1>React</h1>
     </TodoHeader>
   );
